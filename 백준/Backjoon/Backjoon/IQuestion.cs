163118fa8529665ae53e9bc1abd193528f18f3f4
Park ChulHwan <howggas@naver.com>
2023-02-01 23:44:10 +0900

    :memo: 백준 정렬문제 (#16)
    https://www.acmicpc.net/step/9

---
+++ 백준/Backjoon/Backjoon/IQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon
+{
+    internal interface IQuestion
+    {
+        void Apply();
+    }
+}
---
+++ 백준/Backjoon/Backjoon/Q2587.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backjoon
+{
+    internal class Q2587
+    {
+        public void Apply()
+        {
+            List<int> values = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                values.Add(int.Parse(Console.ReadLine()));
+            }
+
+            values.Sort();
+            int average = (int) values.Average();
+            int centerValue = values[2];
+
+            Console.WriteLine(average);
+            Console.WriteLine(centerValue);
+        }
+    }
+}
