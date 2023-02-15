306ad979bda3362464ee1f3b83a5e41a5f22f7a9
ChulHwan-VIRNECT <howggas@naver.com>
2023-02-15 23:42:48 +0900

    :memo: 백준 정렬문제 Q10989

---
+++ 백준/Backjoon/Backjoon/Q10989.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Q10989
+{
+    public void Execute()
+    {
+        using var reader = new System.IO.StreamReader(Console.OpenStandardInput());
+
+        using var print = new System.IO.StreamWriter(Console.OpenStandardOutput());
+
+        int count = int.Parse(reader.ReadLine());
+        int[] numbers = new int[10001];
+
+        for (int i = 0; i < count; i++)
+        {
+            numbers[int.Parse(reader.ReadLine())]++;
+        }
+
+        for (int i = 1; i <= 10000; i++)
+        {
+            for (int j = 0; j < numbers[i]; j++)
+            {
+                print.WriteLine(i);
+            }
+        }
+    }
+}
+
