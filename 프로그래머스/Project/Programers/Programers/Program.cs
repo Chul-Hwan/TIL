ec3bb6a56f28618bbffdc83e00b3d6889bf563a3
Chul-Hwan <howggas@naver.com>
2024-11-05 21:56:11 +0900

    :memo: 모의고사

---
+++ 프로그래머스/Project/Programers/Programers/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Programers;
+
+Console.WriteLine("Hello, World!");
+
+var mockexam = new Mockexam();
+
+Console.WriteLine(string.Join(", ", mockexam.Solution(new int[] { 1, 2, 3, 4, 5 })));
+Console.WriteLine(string.Join(", ", mockexam.Solution(new int[] { 1, 3, 2, 4, 2 })));
