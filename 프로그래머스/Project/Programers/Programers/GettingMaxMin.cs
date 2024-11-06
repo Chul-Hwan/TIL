d3f32707c1bfbeea2734b9281769ccbc951f7d75
Chul-Hwan <howggas@naver.com>
2024-11-06 11:39:42 +0900

    :memo: 최댓값과 최솟값

---
+++ 프로그래머스/Project/Programers/Programers/GettingMaxMin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers
+{
+    internal class GettingMaxMin
+    {
+        public string Solution(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var integerList = s.Split(' ').Select(integer => int.Parse(integer)).ToList();
+            
+            sb.Append(integerList.Min().ToString());
+            sb.Append(' ');
+            sb.Append(integerList.Max().ToString());
+
+            return sb.ToString();
+        }
+    }
+}
