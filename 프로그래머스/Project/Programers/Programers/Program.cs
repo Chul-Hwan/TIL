bb9386dc0a6bd78b4f5f7b76f96a3811bfe1674f
Chul-Hwan <howggas@naver.com>
2024-11-08 10:07:22 +0900

    :memo: 이중우선순위큐

--- 프로그래머스/Project/Programers/Programers/Program.cs
+++ 프로그래머스/Project/Programers/Programers/Program.cs
@@ -7,3 +7,9 @@ var mockexam = new Mockexam();
 
 Console.WriteLine(string.Join(", ", mockexam.Solution(new int[] { 1, 2, 3, 4, 5 })));
 Console.WriteLine(string.Join(", ", mockexam.Solution(new int[] { 1, 3, 2, 4, 2 })));
+
+var doubleRankQueue = new DoubleRankQueue();
+
+var result = string.Join(", ", doubleRankQueue.Solution(new string[] { "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" }));
+
+Console.WriteLine(result);
