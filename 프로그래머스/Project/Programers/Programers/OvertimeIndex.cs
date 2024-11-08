3ecf7bfd6b34f9fc25c0be7575246198c0a1da1f
Chul-Hwan <howggas@naver.com>
2024-11-08 15:55:47 +0900

    :memo: 야근 지수

---
+++ 프로그래머스/Project/Programers/Programers/OvertimeIndex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programers
+{
+    /// <summary>
+    /// 특수케이스를 대응하는 코드를 만들었다면 코드를 수정하고 나서 다시 한 번 확인할 것.
+    /// </summary>
+    internal class OvertimeIndex
+    {
+        public long Solution(int n, int[] works)
+        {
+            int currentIndex = 0;
+            int current = int.MinValue;
+            long answer = 0;
+
+            Array.Sort(works);
+            Array.Reverse(works);
+
+            while ( n > 0 )
+            {
+                currentIndex = currentIndex % works.Length;
+                var nextIndex = currentIndex + 1;
+
+                if (works[currentIndex] != 0)
+                {
+                    current = --works[currentIndex];
+                }
+
+                // nextIndex가 works의 범위를 넘어섰을 때,
+                // 다음 값보다 현재 값이 더 크거나 같을 경우 처음부터 계산하도록 함. 
+                if (nextIndex == works.Length || current >= works[nextIndex])
+                {
+                    currentIndex = 0;
+                }
+                else if (current < works[nextIndex])
+                {
+                    currentIndex = nextIndex;
+                }
+
+                n--;
+            }
+
+            answer = (long) works.Select(i=> Math.Pow(i, 2)).Sum();
+            return answer;
+        }
+    }
+}
--- 프로그래머스/Project/Programers/Programers/Program.cs
+++ 프로그래머스/Project/Programers/Programers/Program.cs
@@ -13,3 +13,12 @@ var doubleRankQueue = new DoubleRankQueue();
 var result = string.Join(", ", doubleRankQueue.Solution(new string[] { "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" }));
 
 Console.WriteLine(result);
+
+#region overtimeIndex
+var overtimeIndex = new OvertimeIndex();
+
+// Console.WriteLine("overtimeIndex : " + overtimeIndex.Solution(999, new int[] { 800, 100, 55, 45 }));
+// Console.WriteLine("overtimeIndex : " + overtimeIndex.Solution(4, new int[] { 5, 5, 1})); // 9 + 9 + 1
+// Console.WriteLine("overtimeIndex : " + overtimeIndex.Solution(4, new int[] { 10, 6, 1})); // 6, 6, 1
+Console.WriteLine("overtimeIndex : " + overtimeIndex.Solution(6, new int[] { 5, 2, 1})); // 요소 중 하나가 0이 되는 케이스
+#endregion
